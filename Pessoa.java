057266c2793f14f6bf6a64e8da01212062ecb3f9
mistervel12 <134335959+mistervel12@users.noreply.github.com>
2023-05-22 18:31:32 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,15 @@
+
+
+package EstudodeCaso2;
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
