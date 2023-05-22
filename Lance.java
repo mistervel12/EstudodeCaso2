8c282960bbd92802cb56b702b6c7be1582e6c065
mistervel12 <134335959+mistervel12@users.noreply.github.com>
2023-05-22 18:30:37 -0300

    Create Lance.java

---
+++ Lance.java
@@ -0,0 +1,17 @@
+
+package EstudodeCaso2;
+
+public class Lance {
+    private Pessoa licitante;
+    private double valor;
+    public Lance(Pessoa licitante, double valor) {
+    this.licitante = licitante;
+    this.valor = valor;
+    }
+    public Pessoa getLicitante() {
+    return licitante;
+    }
+    public double getValor() {
+    return valor;
+    }
+    }
