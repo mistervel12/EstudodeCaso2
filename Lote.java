74dee2e4a75d2da3ca6ce2f7719987b669457f6b
mistervel12 <134335959+mistervel12@users.noreply.github.com>
2023-05-22 18:31:12 -0300

    Create Lote.java

---
+++ Lote.java
@@ -0,0 +1,37 @@
+
+package EstudodeCaso2;
+
+
+public class Lote {
+    private int numero;
+    private String descricao;
+    private Lance maiorLance;
+    public Lote(int numero, String descricao) {
+    this.numero = numero;
+    this.descricao = descricao;
+    }
+    public int getNumero() {
+    return this.numero;
+    }
+    public String getDescricao() {
+    return this.descricao;
+    }
+    public Lance getMaiorLance() {
+    return this.maiorLance;
+    }
+    public void lancePara(Pessoa licitante, double valor) {
+    if (this.maiorLance == null || (this.maiorLance.getValor() < valor)) {
+    this.setMaiorLance(new Lance(licitante, valor));
+    } else {
+    System.out.println("Lote no: " + this.getNumero() + "(" +
+
+    this.getDescricao() + ")" +
+    "Atualmente tem um lance de: " +
+    this.maiorLance.getValor());
+
+    }
+    }
+    private void setMaiorLance(Lance maiorLance) {
+    this.maiorLance = maiorLance;
+    }
+}
